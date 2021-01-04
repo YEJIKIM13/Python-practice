337cc648b6105b9248351e543c425e0175fec0a9
YEJIKIM13 <lerry597@gmail.com>
2021-01-04 16:03:46 +0900

    palindrome solution

--- palindrome.py
+++ palindrome.py
@@ -1,14 +1,14 @@
 # 팰린드롬 여부를 확인하는 함수 is_palindrome을 작성
 # is_palindrome은 파라미터 word가 팰린드롬이면 True를 리턴, 팰린드롬이 아니면 False를 리턴
+# 인덱스를 이용해서 비교할 아이디어
 
 # 함수정의
 def is_palindrome(word):
-    reverse_word_list = list(word)  # list로 형변환
-    reverse_word_list.reverse()  # reverse 정렬
-
-    reverse_word = "".join(reverse_word_list)
-    return (reverse_word == word)
-
+    for left in range(len(word)):
+        right = len(word) - (left+1)
+        if word[left] != word[right]:
+            return False
+    return True  # 들여쓰기 조심할 것
 
 # 테스트
 print(is_palindrome("racecar"))
