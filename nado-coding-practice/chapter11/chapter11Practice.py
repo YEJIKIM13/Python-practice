9769332664bec7cbb2f174497dcd373bba449752
YEJIKIM13 <lerry597@gmail.com>
2021-01-07 10:55:33 +0900

    how to know location of modules

--- nado-coding-practice/chapter11/chapter11Practice.py
+++ nado-coding-practice/chapter11/chapter11Practice.py
@@ -7,9 +7,11 @@
 # trip_to = ThailandPackage()
 # trip_to.detail()
 
+# from travel import *
+# # trip_to = vietnam.VietnamPackage()
+# trip_to = thailand.ThailandPackage()
+# trip_to.detail()
+
+import inspect
 from travel import *
-# trip_to = vietnam.VietnamPackage()
-trip_to = thailand.ThailandPackage()
-trip_to.detail()
-
-
+print(inspect.getfile(thailand))
