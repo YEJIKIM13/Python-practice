bd1331dceb054650737b8eef2516d280a1da3bc6
YEJIKIM13 <lerry597@gmail.com>
2021-01-08 17:11:02 +0900

    youtube_dl 이용해 영상 다운받기(옵션)

--- codeit-practice/codeit-python-advanced/practicePackage.py
+++ codeit-practice/codeit-python-advanced/practicePackage.py
@@ -1,4 +1,10 @@
 import youtube_dl
 
-with youtube_dl.YoutubeDL() as ydl:  # 유튜브 YoutubeDL 이라는 객체를 생성해서 ydl에 저장
+ydl_opt = {
+    # 'listformats': True  # 다운로드 가능한 모든 포맷들 출력, YoutubeDL은 (best)라고 되어있는 파일 다운, 일단 포맷 이걸로 확인하고
+    # 'format' : '18',  # 만약 그 중 18번 포맷 다운하고 싶다면
+
+}
+
+with youtube_dl.YoutubeDL(ydl_opt) as ydl:  # 유튜브 YoutubeDL 이라는 객체를 생성해서 ydl에 저장
     ydl.download(['https://youtu.be/cGC5TESLo9I'])  # 다운받고싶은 영상 주소나 재생목록을 리스트 안에 넣으면 됨
